a42d96ca47c1889070ec9c7b4cb42d236d07e03d
Joshua Rice <jrice@lacrossefootwear.com>
2023-07-21 17:21:53 -0400

    Check whether parameter is handled by UI component

--- Plugin/Magento/Widget/Block/Adminhtml/Widget/Options/DecodeComponentValues.php
+++ Plugin/Magento/Widget/Block/Adminhtml/Widget/Options/DecodeComponentValues.php
@@ -36,16 +36,17 @@ class DecodeComponentValues
         $result,
         string $key
     ) {
-        if ($key !== 'widget_values') {
+        if ($key !== 'widget_values' || empty($result) || !is_array($result)) {
             return $result;
         }
 
-        if (!$result || !is_array($result)) {
-            return $result;
-        }
-
-        foreach ($result as &$value) {
-            $value = $this->decodeComponentValue->execute($value);
+        // Only decode widget parameters that are encoded
+        $config = $subject->getWidgetInstance()->getWidgetConfigAsArray();
+        foreach ($result as $field => &$value) {
+            $helperBlock = $config['parameters'][$field]['helper_block']?? null;
+            if (isset($helperBlock) && $helperBlock['type'] === Components::class) {
+                $value = $this->decodeComponentValue->execute($value);
+            }
         }
 
         return $result;
